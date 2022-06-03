bb91cb1749bf9b138d3426e374f8412cd27bcdb9
keane3pereira <keane3pereira@gmail.com>
2022-06-02 18:19:23 -0700

    Updates

---
+++ precache-manifest.b8d60646d34fd2e24c5e61ce2daf6084.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7209c235f175acaab864",
+    "url": "/static/css/main.13e91da5.chunk.css"
+  },
+  {
+    "revision": "7209c235f175acaab864",
+    "url": "/static/js/main.7209c235.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "2ded161d425ce65a7129",
+    "url": "/static/js/2.2ded161d.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "a2f6b73732cebe3f9c83684d21cca564",
+    "url": "/index.html"
+  }
+];
